cf0bb147aa2fccb7dd62ccc4699c2ae8ba49d164
mjosh51 <mayorjoshua51@gmail.com>
2023-02-15 16:12:52 -0600

    .

--- components/Dashboard/graphs/index.tsx
+++ components/Dashboard/graphs/index.tsx
@@ -3,24 +3,27 @@ import '@ant-design/flowchart/dist/index.css';
 
 const GraphsUI = () => {
   const data = [
-    { year: '1991', value: 3 },
-    { year: '1992', value: 4 },
-    { year: '1993', value: 3.5 },
-    { year: '1994', value: 5 },
-    { year: '1995', value: 4.9 },
-    { year: '1996', value: 6 },
-    { year: '1997', value: 7 },
-    { year: '1998', value: 9 },
-    { year: '1999', value: 13 },
+    { date: 'Jan 4', value: 45 },
+    { date: 'Jan 5', value: 45 },
+    { date: 'Jan 7', value: 50 },
+    { date: 'Jan 9', value: 52 },
+    { date: 'Jan 12', value: 47 },
+    { date: 'Jan 14', value: 49 },
+    { date: 'Jan 16', value: 41 },
+    { date: 'Jan 20', value: 43 },
+    { date: 'Jan 23', value: 45 },
+    { date: 'Jan 25', value: 45 },
+    { date: 'Jan 28', value: 50 },
+    { date: 'Jan 31', value: 48 },
   ];
 
   const config = {
     data,
     height: 400,
-    xField: 'year',
+    xField: 'date',
     yField: 'value',
     point: {
-      size: 5,
+      size: 9,
       shape: 'diamond',
     },
   };
